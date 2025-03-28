f4aa6cd4b8601e7f71c5f8d0f500c3d65eb1dbe8
Seth Harrington <sbgha.09@gmail.com>
2025-03-28 08:55:20 -0600

    Alerts feature implemented

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,14 @@ import {
   updateBreadcrumb,
   loadHeaderFooter,
 } from "./utils.mjs";
+import Alert from "./Alert.mjs";
 
 const dataSource = new ExternalServices("tents");
 const element = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, element);
-
+const alerts = new Alert(".alert-list");
 loadHeaderFooter();
 productList.init();
 updateBreadcrumb();
 // updateCartCount();
+alerts.readAlerts();
